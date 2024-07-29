59541f4dc37f9a12bdf192cb12270fd8288fcda1
Tayyab <m.tayyab33600@gmail.com>
2024-07-29 22:28:40 +0500

    Add data folder for user and quiz data

---
+++ data/quizzes.js
@@ -0,0 +1,34 @@
+module.exports = {
+  quizzes: [
+    {
+      title: "General Knowledge Quiz",
+      questions: [
+        {
+          question: "What is the capital of Pakistan?",
+          options: ["Karachi", "Lahore", "Islamabad", "Rawalpindi"],
+          answer: "Islamabad",
+        },
+        {
+          question: "Who is the founder of Pakistan?",
+          options: [
+            "Allama Iqbal",
+            "Liaquat Ali Khan",
+            "Quaid-e-Azam",
+            "Zulfiqar Ali Bhutto",
+          ],
+          answer: "Quaid-e-Azam",
+        },
+        {
+          question: "When did Pakistan gain independence?",
+          options: ["1945", "1947", "1950", "1955"],
+          answer: "1947",
+        },
+        {
+          question: 'Which city is known as the "City of Lights"?',
+          options: ["Lahore", "Karachi", "Islamabad", "Peshawar"],
+          answer: "Karachi",
+        },
+      ],
+    },
+  ],
+};
---
+++ data/users.js
@@ -0,0 +1,11 @@
+module.exports = {
+    users: [
+        { id: 1, role: 'student', name: 'Tayyab', quizzes: [] },
+        { id: 2, role: 'student', name: 'Khan', quizzes: [] },
+        { id: 3, role: 'student', name: 'Bilal', quizzes: [] },
+        { id: 4, role: 'student', name: 'Nawaz', quizzes: [] },
+        { id: 5, role: 'teacher', name: 'Rehman', quizzes: [] },
+        { id: 6, role: 'teacher', name: 'Asim', quizzes: [] },
+        { id: 7, role: 'admin', name: 'Admin 1' }
+    ]
+};
